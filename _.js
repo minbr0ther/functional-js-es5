12e338d146af2ba30e2ac903f8d14f4085b90ee7
minbr0ther <minbr0ther@naver.com>
2022-01-05 17:08:28 +0900

    _go 예제 수정 및 테스트

--- _.js
+++ _.js
@@ -38,38 +38,6 @@ var _get = _curryr(function (obj, key) {
   return obj === null ? undefined : obj[key];
 });
 
-const _length = _get('length');
-
-function _each(list, iter) {
-  for (var i = 0; i < _length(list); i++) {
-    iter(list[i]);
-  }
-  return list;
-}
-
-const slice = Array.prototype.slice;
-function _rest(list, num) {
-  return slice.call(list, num || 1);
-}
-
-function _reduce(list, iter, memo = 0) {
-  _each(list, (val) => {
-    memo = iter(memo, val);
-  });
-
-  return memo;
-}
-
-function _pipe() {
-  const fns = arguments;
-  return (arg) => _reduce(fns, (arg, fn) => fn(arg), arg);
-}
-
-function _go(arg) {
-  const fns = _rest(arguments);
-  return _pipe.apply(null, fns)(arg);
-}
-
 function _is_object(obj) {
   return typeof obj === 'object' && !!obj;
 }
@@ -77,9 +45,64 @@ function _is_object(obj) {
 function _keys(obj) {
   return _is_object(obj) ? Object.keys(obj) : [];
 }
+const _length = _get('length');
 
-console.log(_keys(null)); // null
-console.log(_keys([1, 2, 3, 4])); // [ '0', '1', '2', '3' ]
+function _each(list, iter) {
+  var keys = _keys(list); //keys는 배열이건 오브젝트건간에 배열을 반환
 
-//const _map = _curryr(_map);
-//const _filter = _curryr(_filter);
+  for (var i = 0, len = keys.length; i < len; i++) {
+    iter(list[keys[i]], keys[i]);
+  }
+
+  return list;
+}
+
+var _map = _curryr(_map),
+  _each = _curryr(_each),
+  _filter = _curryr(_filter);
+
+var slice = Array.prototype.slice;
+function _rest(list, num) {
+  return slice.call(list, num || 1);
+}
+
+function _reduce(list, iter, memo) {
+  if (arguments.length == 2) {
+    memo = list[0];
+    list = _rest(list);
+  }
+  _each(list, function (val) {
+    memo = iter(memo, val);
+  });
+  return memo;
+}
+
+function _pipe() {
+  var fns = arguments;
+  return function (arg) {
+    return _reduce(
+      fns,
+      function (arg, fn) {
+        return fn(arg);
+      },
+      arg
+    );
+  };
+}
+
+function _go(arg) {
+  var fns = _rest(arguments);
+  return _pipe.apply(null, fns)(arg);
+}
+
+_go(
+  {
+    13: 'ID',
+    19: 'HD',
+    29: 'YD',
+  },
+  _map(function (name) {
+    return name.toLowerCase();
+  }),
+  console.log
+); //[ 'id', 'hd', 'yd' ]
